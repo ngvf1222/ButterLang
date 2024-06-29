bad54935f9b2a2835f05d1dfa2a6a58f96a31627
ngvf1222 <yjyj_1222@naver.com>
2024-06-29 12:21:24 +0900

    99bottles text gen update

--- num_to_blang.py
+++ num_to_blang.py
@@ -1,7 +1,7 @@
 def partion_postive(var_,n):
     if n<6:
         return f"{var_} 닥쳐{'!'*n}"
-    for i in range(5,1,-1):
+    for i in range(50,1,-1):
         if n%i==0:
             return f'{partion_postive(var_,int(n/i))}\n{var_} 닥쳐 {var_} {"!"*i}'
     return f'{partion_postive(var_,n-1)}\n{var_} 후우...!'
